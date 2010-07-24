0bf4df1b108eddef8682147c253ac6dea9f0d5d4
Emmanuel Okyere <chief@hutspace.net>
2010-07-24 05:53:25 +0000

    Added preliminary code for recaptcha theme settings (hardcoded to 'clean' theme)

--- recaptcha/views/default/input/captcha.php
+++ recaptcha/views/default/input/captcha.php
@@ -12,8 +12,43 @@
 	global $CONFIG;
 	require_once($CONFIG->pluginspath . "recaptcha/lib/recaptchalib.php");
 
-	$publickey = get_plugin_setting('publickey', 'recaptcha');	
+	$publickey = get_plugin_setting('publickey', 'recaptcha');
+	$theme = 'clean'; //get_plugin_setting('theme', 'recaptcha'); // 'red, white, blackglass, clean'
+	$customize_clean = true;	
+	
+	$widget_bg_color = '#97e2f6';
+	$widget_border_color = '#72DCFA';
+	$input_border_color = '#DFF5FF';
+	$input_bg_color = '#FFFFFF';
+	
+?>
+
+<script type="text/javascript">
+var RecaptchaOptions = {
+   theme : '<?php echo $theme; ?>'
+};
+</script>
+
+<?php 
+if ($customize_clean) {
+	$out  = '<style type="text/css" media="screen">';
+	$out .= '.recaptchatable .recaptcha_image_cell, #recaptcha_table {';
+	$out .=	'  background-color: ' . $widget_bg_color . ' !important;'; //reCaptcha widget background color
+	$out .= '} ';
+	$out .= '#recaptcha_table {';
+	$out .= '  border-color: ' . $widget_border_color . ' !important;'; //reCaptcha widget border color
+	$out .= '} ';
+	$out .= '#recaptcha_response_field {';
+	$out .= '  border-color: ' . $input_border_color . ' !important;'; //Text input field border color
+	$out .= '  background-color: ' . $input_bg_color . ' !important;'; //Text input field background color
+	$out .= '} ';
+	$out .= '</style> ';
+	
+	echo $out;
+}
 ?>
 <div id="recaptcha_div">
 	<?php echo recaptcha_get_html($publickey, $error); ?>
 </div>
+
+	
